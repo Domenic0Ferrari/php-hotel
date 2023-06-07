d509ee5b707be2fb570721149daa51faa77bcd4a
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-07 15:27:32 +0200

    add foreach in foreach

--- index.php
+++ index.php
@@ -46,17 +46,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
 <body>
-    <?php 
-    foreach($hotels as $hotel){
-    ?>
-    <h2>
-        <?php echo $hotel["name"];
-        ?>
-    </h2>
-    <?php
-    }
-    ?>
+    <div class="container">
+        <h1 class="text-center">Ricerca Hotel</h1>
+        <ul>
+            <li>
+                <?php 
+                foreach($hotels as $hotel){
+                    foreach($hotel as $valor){
+                        echo $valor;
+                    }
+                }
+                ?>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
